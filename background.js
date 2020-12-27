c8c95cc30ab3e283a1e0cd9d555b46ba63629a06
chequerNoel <noel@chequer.io>
2020-12-28 03:30:30 +0900

    Chore: Optimize Readability

--- background.js
+++ background.js
@@ -1,32 +1,49 @@
-setInterval(() => {
-  if (window.location.pathname === "/") {
-    [
-      ...[...document.querySelector("main").childNodes[0].children]
-        .map((post) => [...post.children])
-        .map((content) => {
-          let contentIndex = 0;
-          if (content.length === 3) {
-            contentIndex = 1;
-          }
-          return content[contentIndex];
-        })
-        .map((innerContent) => innerContent.children)
-        .map((contents) => [...[...contents][0].children].map((v) => v)),
-    ].map((a) =>
-      a.map((b) => {
-        [
-          "위코드",
-          "wecode",
-          "코드스테이츠",
-          "코드숨",
-          "패스트캠퍼스",
-          "비전공자",
-        ].map(
-          (keyword) =>
-            b.innerText.toLowerCase().includes(keyword) &&
-            b.parentNode.parentNode.parentNode.remove()
+function getRoot(element) {
+    return Array.from(element.childNodes[0].children);
+}
+
+function getChildren(element) {
+    return element.children;
+}
+
+function getContentItem(content) {
+    let contentIndex = 0;
+    if (content.length === 3) {
+        contentIndex = 1;
+    }
+    return content[contentIndex];
+}
+
+function getChildrenArray(contents) {
+    return Array.from(contents[0].children);
+}
+
+function removeIgnoredKeyword(keywords) {
+    return function (element) {
+        keywords.map(
+            (keyword) => {
+                if (new RegExp(keyword, 'i').test(element.innerText)) {
+                    element.parentNode.parentNode.parentNode.remove();
+                }
+            }
         );
-      })
-    );
-  }
+    }
+}
+
+setInterval(() => {
+    if (window.location.pathname === "/") {
+        getRoot(document.querySelector("main"))
+            .map(getChildren)
+            .map(getContentItem)
+            .map(getChildren)
+            .flatMap(getChildrenArray)
+            .forEach(removeIgnoredKeyword([
+                "위코드",
+                "wecode",
+                "코드스테이츠",
+                "코드숨",
+                "패스트캠퍼스",
+                "비전공자",
+            ]));
+    }
 }, 500);
